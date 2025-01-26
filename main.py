7e0f1cb1d259e15272db87b15f4a10d79fe28354
developerpaaji <singhbhavneetdeveloper@gmail.com>
2025-01-26 15:34:58 +0530

    fx

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ def detect_lang():
     res={"lang":max(probe,key=probe.get)}
     return jsonify(res)
 
-# if __name__ == '__main__':
-#     app.run(debug=False,host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0',port=80)
